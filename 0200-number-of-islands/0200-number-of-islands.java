1e9c27b09ec912f6a475035fc1741c9f8003c93a
Sweta1945 <64771851+Sweta1945@users.noreply.github.com>
2022-11-01 15:04:07 +0530

    Time: 19 ms (13.32%), Space: 57.3 MB (55.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,66 +1,47 @@
 class Solution {
-    class pair{
-        int first;
-        int second;
-        public pair(int first, int second)
-        {
-            this.first=first;
-            this.second=second;
-        }
-    }
     public int numIslands(char[][] grid) {
-        
-        int[][] visited=new int[grid.length][grid[0].length];
-        for(int i=0; i<visited.length; i++)
-        {
-            for(int j=0; j<visited[0].length; j++)
-            {
-                visited[i][j]=0;
-            }
-        }
         int count=0;
+        int[][] vis=new int[grid.length][grid[0].length];
         for(int i=0; i<grid.length; i++)
         {
             for(int j=0; j<grid[0].length; j++)
             {
-                if(grid[i][j]=='1' && visited[i][j]!=1)
+                if(vis[i][j]!=1 && grid[i][j]=='1')
                 {
+                    vis[i][j]=1;
+                    bfs(i, j, vis, grid);
                     count++;
-                    bfs(grid, i, j, visited);
                 }
             }
         }
         return count;
-        
     }
-    
-    public void bfs(char[][] grid, int i, int j, int[][]visited)
+    public void bfs(int i, int j, int[][]vis, char[][]grid)
     {
-        int[][] direction = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-        visited[i][j]=1;
-        Queue<pair> q=new ArrayDeque<>();
-        q.add(new pair(i, j));
+        int[][] direction={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vis[i][j]=1;
         
-        while(!q.isEmpty())
+        Queue<int[]> q=new ArrayDeque<>();
+        
+        q.add(new int[]{i, j});
+        
+        while(q.size()>0)
         {
-            int row=q.peek().first;
-            int col=q.peek().second;
-            q.remove();
+            int[] node=q.remove();
+            int i_new=node[0];
+            int j_new=node[1];
             
             for(int[] dir: direction)
             {
-                int x=row+dir[0];
-                int y=col+dir[1];
-                
-                if(x>=0 && x<grid.length && y>=0 && y<grid[0].length && grid[x][y]=='1' && visited[x][y]!=1)
+                int x=i_new+dir[0];
+                int y=j_new+ dir[1];
+                if(x>=0 && x<grid.length && y>=0 && y<grid[0].length && grid[x][y]=='1' && vis[x][y]!=1)
                 {
-                    q.add(new pair(x,y));
-                    visited[x][y]=1;
+                    q.add(new int[]{x,y});
+                    vis[x][y]=1;
                 }
             }
+            
         }
-        
-        
-        
     }
 }
